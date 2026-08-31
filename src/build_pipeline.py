4f40b9cb76ba87bdf5cc48d1d02823dd5b6bfc42
Cassia Nascimento <silvanascimentocassia@gmail.com>
2026-08-31 16:41:44 +0200

    changes in hyperparameters

--- src/build_pipeline.py
+++ src/build_pipeline.py
@@ -21,32 +21,31 @@ def build_classifier(trial: Trial | None, model: str) -> BaseEstimator:
         Returns:
             Scikit-learn model.
     """
-    if model=='Logistic Regression':
+    if model=="Logistic Regression":
         if trial is None:
-            return LogisticRegression(max_iter=500, class_weight='balanced')
+            return LogisticRegression(max_iter=500, class_weight="balanced")
 
-        params={'solver': trial.suggest_categorical('lr_solver', ['lbfgs', 'liblinear', 'newton-cg', 'sag']),
-                'C': trial.suggest_float('lr_C',  1e-3, 100, log=True)}
-        if params['solver']=='liblinear':
-            params['penalty']=trial.suggest_categorical('lr_penalty',  ['l1','l2'])
+        params={"solver": trial.suggest_categorical("lr_solver", ["lbfgs", "liblinear", "newton-cg", "sag"]),
+                "C": trial.suggest_float("lr_C",  0.1, 40, log=True)}
+        if params["solver"]=="liblinear":
+            params["penalty"]=trial.suggest_categorical("lr_penalty",  ["l1","l2"])
 
-        return LogisticRegression(max_iter=1500, random_state=RANDOM_STATE, class_weight='balanced',**params)
+        return LogisticRegression(max_iter=500, random_state=RANDOM_STATE, class_weight="balanced",**params)
 
-    elif model=='K-Nearest Neighbors':
+    elif model=="K-Nearest Neighbors":
         if trial is None:
-            return KNeighborsClassifier(n_neighbors=5, metric='minkowski')
+            return KNeighborsClassifier(n_neighbors=5, weights= "uniform", metric="minkowski")
 
-        params={"n_neighbors": trial.suggest_int("knn_n_neighbors", 2, 50),
-                "weights": trial.suggest_categorical("knn_weights",["uniform", "distance"]),
-                "metric": trial.suggest_categorical("knn_metric",["euclidean","manhattan","minkowski"])}
-        if params["metric"]=="minkowski":
-            params["p"]=trial.suggest_float("knn_p",1,5)
-
+        params={"weights": "uniform",
+                "metric": "minkowski",
+                "n_neighbors": trial.suggest_int("knn_n_neighbors", 2, 25),
+                "p": trial.suggest_int("k_nn_p",1,6)}
+        
         return KNeighborsClassifier(**params)
 
-    elif model=='Support Vector Machine':
+    elif model=="Support Vector Machine":
         if trial is None:
-            return SVC(kernel='rbf', probability=True, class_weight='balanced',random_state=RANDOM_STATE)
+            return SVC(kernel="rbf", probability=True, class_weight="balanced",random_state=RANDOM_STATE)
 
         params={"C": trial.suggest_float("svc_C",1e-3,100,log=True),
                 "kernel":trial.suggest_categorical("svc_kernel",["linear","rbf","poly"])}
@@ -56,55 +55,50 @@ def build_classifier(trial: Trial | None, model: str) -> BaseEstimator:
         if params["kernel"] == ["poly"]:
             params["degree"]=trial.suggest_int("svc_degree",2,6)
 
-        return SVC(probability=True, class_weight='balanced',random_state=RANDOM_STATE,**params)
+        return SVC(probability=True,random_state=RANDOM_STATE, class_weight="balanced",**params)
 
-    elif model=='Decision Tree':
+    elif model=="Decision Tree":
         if trial is None:
-            return DecisionTreeClassifier(criterion='gini', random_state=RANDOM_STATE, class_weight='balanced')
+            return DecisionTreeClassifier(criterion="gini", random_state=RANDOM_STATE, class_weight="balanced")
 
         params={"criterion": trial.suggest_categorical("dt_criterion", ["gini", "entropy"]),
-                "max_depth": trial.suggest_int("dt_max_depth", 1, 50),
-                "min_samples_split": trial.suggest_float("dt_min_samples_split", 0., 1.),
-                "min_samples_leaf": trial.suggest_float("dt_min_samples_leaf", 0., 1.),
+                "max_depth": trial.suggest_int("dt_max_depth", 2, 10),
+                "min_samples_split": trial.suggest_int("dt_min_samples_split", 2, 30),
+                "min_samples_leaf": trial.suggest_int("dt_min_samples_leaf", 1, 20),
                 "max_features": trial.suggest_categorical("dt_max_features", [None,"sqrt","log2"])}
 
-        return DecisionTreeClassifier(random_state=RANDOM_STATE, class_weight='balanced',**params)
+        return DecisionTreeClassifier(random_state=RANDOM_STATE, class_weight="balanced",**params)
 
-    elif model=='Random Forest':
+    elif model=="Random Forest":
         if trial is None:
-            return RandomForestClassifier(n_estimators=100, random_state=RANDOM_STATE, class_weight='balanced')
+            return RandomForestClassifier(n_estimators=100, random_state=RANDOM_STATE, class_weight="balanced")
 
-        params={"n_estimators": trial.suggest_int("rf_n_estimators", 100, 1000, step=100),
+        params={"n_estimators": trial.suggest_int("rf_n_estimators", 500, 1500, step=100),
                 "criterion": trial.suggest_categorical("rf_criterion", ["gini", "entropy"]),
-                "max_depth": trial.suggest_int("rf_max_depth", 5, 50),
-                "min_samples_split": trial.suggest_int("rf_min_samples_split", 2, 20),
+                "max_depth": trial.suggest_int("rf_max_depth", 2, 15),
+                "min_samples_split": trial.suggest_int("rf_min_samples_split", 2, 25),
                 "min_samples_leaf": trial.suggest_int("rf_min_samples_leaf", 1, 10),
                 "max_features": trial.suggest_categorical("rf_max_features", ["sqrt","log2", None]),
                 "bootstrap": trial.suggest_categorical("rf_bootstrap", [True, False])}
 
-        return RandomForestClassifier(random_state=RANDOM_STATE, class_weight='balanced',**params)
+        return RandomForestClassifier(random_state=RANDOM_STATE, class_weight="balanced",**params)
 
-    elif model=='XGBoost':
+    elif model=="XGBoost":
         if trial is None:
-            return XGBClassifier(n_estimators =1000, random_state=RANDOM_STATE,n_jobs=1)
+            return XGBClassifier(objective= "binary:logistic", eval_metric="auc", random_state=RANDOM_STATE, n_estimators =1000 ,n_jobs=1)
 
-        params={"objective": "binary:logistic",  # or reg:squarederror
-                "eval_metric": "auc",
-                "booster": "gbtree",
-                "tree_method": "hist",
-                "learning_rate": trial.suggest_float("xgb_learning_rate", 1e-3, 0.1, log=True),
-                "max_depth": trial.suggest_int("xgb_max_depth", 3, 10),
-                "min_child_weight": trial.suggest_float("xgb_min_child_weight", 1, 20),
-                "gamma": trial.suggest_float("xgb_gamma", 0, 10),
-                "subsample": trial.suggest_float("xgb_subsample", 0.5, 1.0),
+        params={"n_estimators": trial.suggest_int("xgb_n_estimators",500,1500, step=100),
+                "scale_pos_weight": trial.suggest_float("xgb_scale_pos_weight", 1., 4.5),
+                "learning_rate": trial.suggest_float("xgb_learning_rate", 0.005, 0.1, log=True),
+                "max_depth": trial.suggest_int("xgb_max_depth", 2, 20),
+                "min_child_weight": trial.suggest_int("xgb_min_child_weight", 1, 20),
+                "gamma": trial.suggest_float("xgb_gamma", 0, 30),
+                "subsample": trial.suggest_float("xgb_subsample", 0.35, 1.0),
                 "colsample_bytree": trial.suggest_float("xgb_colsample_bytree", 0.5, 1.0),
-                "colsample_bylevel": trial.suggest_float("xgb_colsample_bylevel", 0.5, 1.0),
-                "reg_alpha": trial.suggest_float("xgb_reg_alpha", 1e-8, 100, log=True),
-                "reg_lambda": trial.suggest_float("xgb_reg_lambda", 1e-8, 100, log=True),
-                "scale_pos_weight": trial.suggest_float("xgb_scale_pos_weight", 0.5, 20),
-                "n_jobs":1}
+                "reg_alpha": trial.suggest_float("xgb_reg_alpha", 1e-3, 5, log=True),
+                "reg_lambda": trial.suggest_float("xgb_reg_lambda", 1e-3, 5, log=True)}
 
-        return XGBClassifier(n_estimators =1000, random_state=RANDOM_STATE,**params)
+        return XGBClassifier(objective= "binary:logistic", eval_metric="auc", random_state=RANDOM_STATE,n_jobs=1,**params)
 
     raise ValueError(f"Invalid model: {model}")
 
@@ -119,13 +113,13 @@ def build_preprocessor(include_scaler:bool=True) -> ColumnTransformer:
             Scikit-learn column transformer.
     """
 
-    transformers=[('encoder_binary', OneHotEncoder(drop='if_binary'), BINARY_FEATURES),
-        ('encoder_categorical', OneHotEncoder(), CATEGORICAL_FEATURES)]
+    transformers=[("encoder_binary", OneHotEncoder(drop="if_binary"), BINARY_FEATURES),
+        ("encoder_categorical", OneHotEncoder(), CATEGORICAL_FEATURES)]
 
     if include_scaler is True:
-        transformers.append(('scaler_numerical', StandardScaler(), NUMERICAL_FEATURES))
+        transformers.append(("scaler_numerical", StandardScaler(), NUMERICAL_FEATURES))
 
-    return ColumnTransformer(transformers=transformers, remainder='passthrough')
+    return ColumnTransformer(transformers=transformers, remainder="passthrough")
     
 def build_pipeline(trial: Trial | None, model: str) -> Pipeline:
     """ Build a scikit-learn classification pipeline
@@ -139,7 +133,7 @@ def build_pipeline(trial: Trial | None, model: str) -> Pipeline:
     preprocessor=build_preprocessor()
     classifier=build_classifier(trial,model)
 
-    return Pipeline(steps=[('preprocessor', preprocessor),('classifier', classifier)])
+    return Pipeline(steps=[("preprocessor", preprocessor),("classifier", classifier)])
 
 
 def objective(trial: Trial, X: pd.DataFrame, y: pd.Series, model: str, scoring: str = "f1", n_splits: int=5) -> float:
@@ -158,5 +152,5 @@ def objective(trial: Trial, X: pd.DataFrame, y: pd.Series, model: str, scoring: 
 
     cv = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=RANDOM_STATE)
     scores = cross_val_score(pipeline, X, y, cv=cv, scoring=scoring, n_jobs=-1)
-
+    
     return scores.mean()
--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,4 +1,6 @@
-from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, precision_score, recall_score, average_precision_score, confusion_matrix, classification_report
+from sklearn.metrics import roc_curve, accuracy_score, roc_auc_score, f1_score, precision_score, recall_score, average_precision_score, confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import numpy as np
 import logging
 
 logger = logging.getLogger(__name__)
@@ -21,15 +23,25 @@ def evaluate_model(fitted_pipeline, X_test, y_test):
     classifier_name=type(fitted_pipeline.named_steps['classifier']).__name__
 
     metrics={'classifier_name':classifier_name,
-             'accuracy': accuracy_score(y_test, y_pred),
-             'roc_auc': roc_auc_score(y_test, y_prob),
-             'f1': f1_score(y_test, y_pred),
-             'precision': precision_score(y_test, y_pred),
-             'recall': recall_score(y_test, y_pred),
-             'average_precision': average_precision_score(y_test, y_prob),
-             'confusion_matrix': confusion_matrix(y_test, y_pred),
-             'report': classification_report(y_test, y_pred, target_names=['Not Churn', 'Churn'], output_dict=False)}
+             'accuracy': round(accuracy_score(y_test, y_pred),3),
+             'f1': round(f1_score(y_test, y_pred),3),
+             'precision': round(precision_score(y_test, y_pred),3),
+             'recall': round(recall_score(y_test, y_pred),3),
+             'roc_auc': round(roc_auc_score(y_test, y_prob),3),
+             'average_precision': round(average_precision_score(y_test, y_prob),3),
+             'confusion_matrix': confusion_matrix(y_test, y_pred)}
+    report= classification_report(y_test, y_pred, target_names=['Not Churn', 'Churn'], output_dict=False)
 
     logger.info(f"Calculated accuracy, roc_auc, f_1, precision, recall, average_precision, confusion_matrix and report for {classifier_name} classification model.")
 
-    return metrics
+    fpr, tpr, thresholds = roc_curve(y_test, y_prob, pos_label=1)
+    plt.plot(fpr, tpr,color="red")
+    plt.fill_between(fpr, tpr,color="red",alpha=0.2)
+    plt.plot(np.linspace(0.,1,10), np.linspace(0.,1,10),"--",color="black")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positie Rate")
+    plt.title("ROC curve")
+    plt.text(0.05, 0.95, f'AUC: {metrics["roc_auc"]}', fontsize=12,
+        verticalalignment='top', bbox={'boxstyle':'round', 'facecolor':'white', 'alpha':0.5})
+    plt.show()
+    return metrics,report
